81dd7979a46a1368c87253edbf421009a90a2775
Drorka~ <drorpa4@gmail.com>
2023-01-10 16:46:29 +0200

    fix add btn position

--- src/cmps/side-bar.jsx
+++ src/cmps/side-bar.jsx
@@ -4,7 +4,7 @@ import { ToyFilter } from './toy-filter'
 export function SideBar({ onSetFilter, onSetSort }) {
 	return (
 		<aside className="side-bar">
-			<div className="side-bar-container">
+			<div className="side-bar-container flex column">
 				<button className="add-btn">
 					<Link to={`/toy/edit`}>Add Toy</Link>
 				</button>
